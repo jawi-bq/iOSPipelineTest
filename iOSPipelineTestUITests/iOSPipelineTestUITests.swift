33ae3be7d8e50398575cdc6367b0e4a25f2d8ef3
Jacek Winnicki <jacek.winnicki@britenet.com.pl>
2020-01-31 08:01:28 +0100

    TextField test.

--- iOSPipelineTestUITests/iOSPipelineTestUITests.swift
+++ iOSPipelineTestUITests/iOSPipelineTestUITests.swift
@@ -30,14 +30,32 @@ class iOSPipelineTestUITests: XCTestCase {
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        app.textFields["loginInput"].tap()
+        
+        let tKey = app/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboards.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        tKey.tap()
+        tKey.tap()
+        
+        let eKey = app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        eKey.tap()
+        eKey.tap()
+        
+        let sKey = app/*@START_MENU_TOKEN@*/.keys["s"]/*[[".keyboards.keys[\"s\"]",".keys[\"s\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        sKey.tap()
+        sKey.tap()
+        tKey.tap()
+        tKey.tap()
+        app/*@START_MENU_TOKEN@*/.buttons["loginButtom"]/*[[".buttons[\"Button\"]",".buttons[\"loginButtom\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
     }
 
-    func testLaunchPerformance() {
+    /*func testLaunchPerformance() {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
             measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
                 XCUIApplication().launch()
             }
         }
-    }
+    }*/
 }
